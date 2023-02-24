cf054b1b08a72e984fcd8617c015c2462171a902
falsai <filipe@cast.ai>
2023-02-24 16:10:55 +0100

    bump provider to v3.0.0

--- versions.tf
+++ versions.tf
@@ -2,12 +2,13 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
-    aws    = {
+    aws = {
       source  = "hashicorp/aws"
       version = ">= 2.49"
     }
     castai = {
       source  = "castai/castai"
+      version = ">= 3.0.0"
     }
   }
 }
